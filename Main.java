d3467eaffc7fce6bbfa0ea78c575363c7f470754
Đoàn Văn Duy <163749596+Duyy0702@users.noreply.github.com>
2024-04-10 10:49:53 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,162 @@
+import java.io.*;
+import java.util.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.*;
+import java.security.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.math.BigInteger;
+
+class Student {
+    int id;
+    String name;
+    String address;
+    String dateOfBirth;
+    int age;
+    int sum;
+    boolean isDigit;
+
+    public Student(int id, String name, String address, String dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+
+	public boolean isDigit() {
+		return isDigit;
+	}
+
+	public void setDigit(boolean isDigit) {
+		this.isDigit = isDigit;
+	}
+    
+}
+
+
+class Thread4 implements Runnable {
+    List<Student> students;
+
+    public Thread4(List<Student> students) {
+        this.students = students;
+    }
+
+    public void run() {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.newDocument();
+
+            Element root = doc.createElement("students");
+            doc.appendChild(root);
+
+            for (Student student : students) {
+                Element studentElement = doc.createElement("student");
+                root.appendChild(studentElement);
+
+                Element idElement = doc.createElement("id");
+                idElement.setTextContent(Integer.toString(student.id));
+                studentElement.appendChild(idElement);
+
+                Element nameElement = doc.createElement("name");
+                nameElement.setTextContent(student.name);
+                studentElement.appendChild(nameElement);
+
+                Element addressElement = doc.createElement("address");
+                addressElement.setTextContent(student.address);
+                studentElement.appendChild(addressElement);
+
+                Element ageElement = doc.createElement("age");
+                ageElement.setTextContent(Integer.toString(student.age));
+                studentElement.appendChild(ageElement);
+
+                Element sumElement = doc.createElement("sum");
+                sumElement.setTextContent(Integer.toString(student.sum));
+                studentElement.appendChild(sumElement);
+
+                Element isDigitElement = doc.createElement("isDigit");
+                isDigitElement.setTextContent(Boolean.toString(student.isDigit));
+                studentElement.appendChild(isDigitElement);
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("./src/kq.xml"));
+
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        List<Student> students = new ArrayList<>();
+        Thread t1 = new Thread(new Thread1(students));
+        Thread t2 = new Thread(new Thread2(students));
+        Thread t3 = new Thread(new Thread3(students));
+        Thread t4 = new Thread(new Thread4(students));
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+        t3.start();
+		t3.join();			
+        t4.start();
+    }
+}
---
+++ Thread3.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+class Thread3 implements Runnable {
+    List<Student> students;
+
+    public Thread3(List<Student> students) {
+        this.students = students;
+    }
+    public void run() {
+        for (Student student : students) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(student.sum); i++) {
+                if (student.sum % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+    }
+}
+
